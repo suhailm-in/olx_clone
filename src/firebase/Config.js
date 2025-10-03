24b431da31138c8236602819f8081fb32c474238
Suhail M <suhailm.online@gmail.com>
2025-10-03 21:42:48 +0530

    Add GitHub Actions deploy.yml for GitHub Pages

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCW7CyydaGmozYA_tXWZCIXX41YIaaWJTg",
-  authDomain: "olx-app-87e41.firebaseapp.com",
-  projectId: "olx-app-87e41",
-  storageBucket: "olx-app-87e41.firebasestorage.app",
-  messagingSenderId: "872656521943",
-  appId: "1:872656521943:web:342b811b0fd4770d89fe92",
-  measurementId: "G-PEPQMJ87NS"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
