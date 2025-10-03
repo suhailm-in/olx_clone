f035e63854beed63e41164117196ea021f3039fe
Suhail M <suhailm.online@gmail.com>
2025-10-04 00:01:46 +0530

    add custom SEO and structured data for Create Product page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,7 +8,7 @@ import Footer from "../Components/Footer/Footer";
 import CustomSEO from "../includes/CustomSEO";
 
 function Home(props) {
-	
+
     // JSON-LD structured data for Home (like a Website)
     const jsonLd = {
         "@context": "https://schema.org",
@@ -25,7 +25,7 @@ function Home(props) {
                 title="OLX Clone | Buy & Sell Products Easily"
                 description="OLX Clone lets you buy and sell cars, mobiles, electronics, and more."
                 url={window.location.href}
-                image="https://yourdomain.com/assets/og-image.png" // Optional OG image
+                image="https://raw.githubusercontent.com/suhailm-in/olx_clone/master/screenshots/Screenshot_home.png" // Optional OG image
                 type="website"
                 jsonLd={jsonLd}
             />
